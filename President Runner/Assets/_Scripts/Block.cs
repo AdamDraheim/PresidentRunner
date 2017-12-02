59f10da40ca78aa9303aac836344fcdf19d0f95b
Knightrealm <acdraheim@gmail.com>
2017-12-02 01:01:49 -0700

    Base Game
    Uploading Base

---
+++ President Runner/Assets/_Scripts/Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (player.transform.position.x - transform.position.x < -500)
+        {
+            transform.position = new Vector3(500, 0, 0);
+        }
+
+	}
+}
---
+++ President Runner/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float SPEED = 5;
+    public float STARTING_SPEED = 0.1f;
+    private Vector3 movement;
+    public bool jump = false;
+    private float jumpSpeed = 0;
+
+    private float GRAVITY = 0.2f;
+    private float JUMP_SPEED = 0.3f;
+    private float SPEED_INCREASE = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+
+        movement = new Vector3(0, SPEED, 0);
+        SPEED = STARTING_SPEED;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        SPEED += (SPEED_INCREASE * Time.deltaTime);
+
+        if (Input.GetKey("up") && !jump)
+        {
+            jump = true;
+            jumpSpeed = JUMP_SPEED;
+        }
+
+        if (jump)
+        {
+            jumpSpeed -= (GRAVITY * Time.deltaTime);
+        }
+        else
+        {
+            jumpSpeed = 0;
+        }
+
+
+        movement = new Vector3(SPEED, jumpSpeed, 0);
+
+        transform.position += (movement * Time.deltaTime);
+
+	}
+}
